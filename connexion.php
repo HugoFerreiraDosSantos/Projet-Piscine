03e8bdc82431a67115eaa3286c1fc1337d222665
AUDE-PC\Aude <aude.le-breton@edu.ece.fr>
2018-04-30 15:03:54 +0200

    Menu de connexion

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<!-- en-têtes de fichier html -->
+<!DOCTYPE html>
+<html>
+
+<!-- section principale avec titre -->
+ <head>
+ <META charset = utf-8" />
+ <title>Social Media Professionel</title>
+ </head>
+ 
+ <!-- section du corps (body) -->
+ <body>
+ 
+	<h2> Connexion </h2>
+ 
+	<form>
+	<label> Pseudo :</label>
+	<input type = "text" name = "pseudo"/> </br> </br>
+	<label> Email :</label>
+	<input type = "mail" name = "email"/> </br> </br>
+	
+	<input type = "submit" value = "Connexion" name = "bouton"/>
+	
+	</form>
+ 
+ </body>
+ 
+ 
+</html>
+<!-- fin de l'index.html -->
+ 
